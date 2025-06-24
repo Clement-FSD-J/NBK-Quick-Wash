66891a6a2cc007614ef2015484146f5a127d82b3
Clement <clement@Clements-MacBook-Air.local>
2025-06-24 19:30:59 +0530

    Our Process checking

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// Toggle nav menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav_menu');
+const closeBtn = document.getElementById('closeBtn');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('show');
+});
+
+
 const slider = document.getElementById("slider");
 const scrollLeft = document.getElementById("scrollLeft");
 const scrollRight = document.getElementById("scrollRight");
